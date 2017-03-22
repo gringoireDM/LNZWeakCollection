4869710763e64176e91165bf1972f029aa9afa15
Giuseppe Lanza <gringoire986@gmail.com>
2017-03-22 08:24:49 +0000

    Initial Commit

---
+++ LNZWeakCollection/LNZWeakCollection.h
@@ -0,0 +1,19 @@
+//
+//  LNZWeakCollection.h
+//  LNZWeakCollection
+//
+//  Created by Giuseppe Lanza on 22/03/17.
+//  Copyright © 2017 Giuseppe Lanza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LNZWeakCollection.
+FOUNDATION_EXPORT double LNZWeakCollectionVersionNumber;
+
+//! Project version string for LNZWeakCollection.
+FOUNDATION_EXPORT const unsigned char LNZWeakCollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LNZWeakCollection/PublicHeader.h>
+
+
